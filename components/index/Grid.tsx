3cd9c9845fc23e05a09a45efc1ea0d4392475bd1
Emmanuel Hernandez <e805177@gmail.com>
2023-08-21 12:17:39 -0600

    Update Grid.tsx

--- components/index/Grid.tsx
+++ components/index/Grid.tsx
@@ -6,25 +6,25 @@ const Grid = ({ components, users }: Props) => {
   return (
     <div className="bg-cyan-900">
       <div className="grid md:grid-cols-3 gap-3 max-w-5xl mx-auto py-40 container">
-        <div className="col-span-2 pt-10 rounded-3xl dark:text-white justify-center text-center py-2 text-3xl bg-cyan-700">
+        <div className="col-span-2 pt-10 rounded-3xl text-white justify-center text-center py-2 text-3xl bg-cyan-700">
           <span className="font-bold text-6xl">+{totalComponents}</span>
           <p className="py-6 text-xl text-center font-light text-gray-200">
             Community-Crafted UI Elements
           </p>
         </div>
-        <div className="pt-10 rounded-3xl dark:text-white justify-center text-center py-2 text-3xl bg-cyan-700">
+        <div className="pt-10 rounded-3xl text-white justify-center text-center py-2 text-3xl bg-cyan-700">
           <span className="font-bold text-6xl">100%</span>
           <p className="py-6 text-xl text-center font-light text-gray-200">
             Free for Personal and Commercial Use
           </p>
         </div>
-        <div className="pt-10 rounded-3xl dark:text-white justify-center text-center py-2 text-3xl bg-cyan-700">
+        <div className="pt-10 rounded-3xl text-white justify-center text-center py-2 text-3xl bg-cyan-700">
           <span className="font-bold text-6xl">100%</span>
           <p className="py-6 text-xl text-center font-light text-gray-200">
             Free for Personal and Commercial Use
           </p>
         </div>
-        <div className="pt-10 rounded-3xl dark:text-white justify-center text-center py-2 text-3xl col-span-2  bg-cyan-700">
+        <div className="pt-10 rounded-3xl text-white justify-center text-center py-2 text-3xl col-span-2  bg-cyan-700">
           <span className="font-bold text-6xl">+{totalUsers}</span>
           <p className="py-6 text-xl text-center font-light text-gray-200">
             Contributors
